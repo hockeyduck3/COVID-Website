10f100b59c013d9846a886a9c2a478db8ecd8486
LJ Spencer <hockeyduck3@gmail.com>
2020-04-08 21:37:28 -0600

    Added some more html and added more functionality

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -21,4 +21,27 @@ function search () {
     $.ajax(settings).done(function (response) {
         console.log(response);
     });
+
+    $.ajax({
+        url:"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=COVID-19&api-key=fba9vvYnRyI2O33HRL1AhwLy6ywpxVpH",
+        method: 'GET'
+    }).then(function (response2) {
+        console.log(response2)
+    })
 }
+// Image
+var settings3 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://coronavirus-monitor.p.rapidapi.com/coronavirus/random_masks_usage_instructions.php",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+		"x-rapidapi-key": "e437273f0fmshecdb698a70bb50fp155d37jsn8b4b323d67aa"
+	}
+}
+
+$.ajax(settings3).done(function (response3) {
+    // var img = $('<img>').attr('src', response3);
+    $(document.body).append(response3)
+});
