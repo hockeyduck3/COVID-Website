f97238033878684a5b3e829950550985f1a3dc9d
LJ Spencer <hockeyduck3@gmail.com>
2020-04-11 12:10:52 -0600

    Added a click event for the city buttons, and made a new function to help dry up the code

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,10 +1,25 @@
 var userInput;
 
-$('.button').click(function () {
-    userInput = $('#search').val();
+$('.button').click(clickSearch)
 
-    search();
-})
+$('.cityBtn').click(clickSearch)
+
+function clickSearch (event) {
+    console.log(event.target)
+
+    if ($(event.target).hasClass('cityBtn')) {
+        userInput = $(event.target).text();
+        search();
+    } else {
+        if ($('#search').val() === '') {
+            alert('Field cannot be empty')
+        } else {
+            userInput = $('#search').val();
+            search();
+        }
+    }
+
+}
 
 function search () {
     var settings = {
@@ -17,20 +32,21 @@ function search () {
             "x-rapidapi-key": "61b6db55b3msh35943488960f57dp1ace38jsn832bfa80a52f"
         }
     }
+
     $.ajax(settings).then(function (response) {
         console.log(response);
         var newCases = response.response[0].cases.new
-     $(".newCases").text("New Cases Today: " + newCases) 
-     var activeCases = response.response[0].cases.active
-     $(".activeCases").text("Total Active Cases: " + activeCases) 
-     var recovered = response.response[0].cases.recovered
-     $(".recovered").text("Total Recovered: " + recovered) 
-     var todaysDeaths = response.response[0].deaths.new
-     $(".todaysDeaths").text("New Deaths Today: " + todaysDeaths) 
-     var totalDeaths = response.response[0].deaths.total
-     $(".totalDeaths").text("Total Deaths: " + totalDeaths) 
-     var testTotal = response.response[0].tests.total
-     $(".testTotal").text("Amount of Tests Done: " + testTotal) 
+        $(".newCases").text("New Cases Today: " + newCases) 
+        var activeCases = response.response[0].cases.active
+        $(".activeCases").text("Total Active Cases: " + activeCases) 
+        var recovered = response.response[0].cases.recovered
+        $(".recovered").text("Total Recovered: " + recovered) 
+        var todaysDeaths = response.response[0].deaths.new
+        $(".todaysDeaths").text("New Deaths Today: " + todaysDeaths) 
+        var totalDeaths = response.response[0].deaths.total
+        $(".totalDeaths").text("Total Deaths: " + totalDeaths) 
+        var testTotal = response.response[0].tests.total
+        $(".testTotal").text("Amount of Tests Done: " + testTotal) 
     });
 
 
