e585f7c11961a971861abc0eb9f6c1ba3dd2cb25
LJ Spencer <hockeyduck3@gmail.com>
2020-04-11 10:53:51 -0600

    Made the articles show up, and added a class of cityBtn to the cities

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -30,5 +30,19 @@ function search () {
         method: 'GET'
     }).then(function (response2) {
         console.log(response2)
+
+        $(response2.response.docs).each(function (e) {
+            var div = $('<div>');
+
+            var articleTitle = $('<h4>').text(response2.response.docs[e].abstract);
+
+            var articleLink = $('<a>').text('Article Link');
+            articleLink.attr('href', response2.response.docs[e].web_url);
+            articleLink.attr('target', '_blank')
+
+            div.append(articleTitle, articleLink);
+
+            $('.articleSection').append(div)
+        })
     })
 }
